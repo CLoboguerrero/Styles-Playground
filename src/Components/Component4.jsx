78836a2683c41e0bf2a2707a609d625e348c8901
CLoboguerrero <120610284+CLoboguerrero@users.noreply.github.com>
2024-03-19 21:21:22 -0600

    Component for scroll animation and styles

--- src/Components/Component4.jsx
+++ src/Components/Component4.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import '../Styles/Component4.modules.css';
+import logo from '../assets/react.svg'
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Component4 = () => {
+
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const el = imgRef.current;
+    gsap.fromTo(el, {rotation: 0},{rotation: 180, scrollTrigger: {
+      trigger: el
+    }})
+  },[])
+
   return (
-    <div>
-      
+    <div className='intro-container-left-4'>
+      <div className='module-border-wrap-4'>
+        <div className='module-4'>
+        </div>
     </div>
+    {/* <div className='helper'></div> */}
+    <img src={logo} alt='logo' className='App-logo' ref={imgRef} />
+</div>
   )
 }
 
