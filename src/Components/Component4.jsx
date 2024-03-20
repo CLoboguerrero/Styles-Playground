2cf3d81f990426579d1d691f39dbf54c5ff83008
CLoboguerrero <120610284+CLoboguerrero@users.noreply.github.com>
2024-03-20 16:38:29 -0600

    Component 4 config with LocomotiveScroll. Testing

--- src/Components/Component4.jsx
+++ src/Components/Component4.jsx
@@ -2,9 +2,14 @@ import '../Styles/Component4.modules.css';
 import logo from '../assets/react.svg'
 import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+//import { ScrollSmoother } from 'gsap/ScrollSmoother';
+//import { ScrollSmoother } from 'gsap/all';
+import { ScrollSmoother } from 'gsap-trial/ScrollSmoother';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother);
+//gsap.registerPlugin(ScrollSmoother);
 
 const Component4 = () => {
 
@@ -12,7 +17,7 @@ const Component4 = () => {
 
   useEffect(() => {
     const el = imgRef.current;
-    gsap.fromTo(el, {rotation: 0},{rotation: 180, scrollTrigger: {
+    gsap.fromTo(el, {rotation: 0},{rotation: 180, duration: 3, scrollTrigger: {
       trigger: el
     }})
   },[])
