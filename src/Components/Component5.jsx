c49c5dfa88cea05d532a0f54c31c70dad6ce5492
CLoboguerrero <120610284+CLoboguerrero@users.noreply.github.com>
2024-03-22 14:26:52 -0600

    Proper React-Lenis component structure with safe animations and cleanup

--- src/Components/Component5.jsx
+++ src/Components/Component5.jsx
@@ -7,19 +7,37 @@ import { useGSAP } from '@gsap/react';
 const Component5 = () => {
 
     const clickBox = useRef();
+    const atomImg = useRef();
 
+    //contextSafe sirve para generar animaciones 'seguras' en REACT para 'limpiar' (de la memoria) la animación una vez que useGSAP se haya montado y que la animación se haya ejecutado.
+    
     const { contextSafe } = useGSAP({scope: clickBox})
 
     const clickToEnlarge = contextSafe (() => {
-        clickBox.current.addEventListener('click', () => {
-            gsap.to('.box', {scale: 1.5});
-        });
+        gsap.to('.component5-logo', {scale: 1.5, rotation: 90});
     });
 
 
+    //Otra manera de poder crear una animación es con eventListener, aunque esto NO es buena práctica de uso en React (Recordar que al estar definitido el tipo de event, no es necesario definir un onClick en el elemento HTML correspondiente)
+
+    // useGSAP((context, contextSafe) => {
+
+    //   const clickToEnlarge2 = contextSafe(() => {
+    //     gsap.to(atomImg.current, {scale: 1.5, rotation: 180});
+    //   });
+
+    //   atomImg.current.addEventListener('click', clickToEnlarge2);
+
+    //   return () => {
+    //     atomImg.current.removeEventListener('click', clickToEnlarge2);
+    //   };
+
+    // }, {scope: clickBox});
+
+
   return (
-    <div className='box'>
-      <img src={logo} alt='logo' className='App-logo' ref={clickBox} onClick={clickToEnlarge}/>
+    <div className='box' ref={clickBox}>
+      <img src={logo} alt='logo' className='component5-logo' onClick={clickToEnlarge} ref={atomImg}/>
     </div>
   )
 }
