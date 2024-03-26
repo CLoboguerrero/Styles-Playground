a72acc9cc1f98eab73eff224c0677959d665cd6a
CLoboguerrero <120610284+CLoboguerrero@users.noreply.github.com>
2024-03-25 19:49:09 -0600

    Use of scrollTrigger with GSAP and react

--- src/Components/Component5.jsx
+++ src/Components/Component5.jsx
@@ -3,11 +3,15 @@ import gsap from 'gsap';
 import logo from '../assets/react.svg'
 import { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Component5 = () => {
 
     const clickBox = useRef();
     const atomImg = useRef();
+    const scrollCuadrito = useRef();
 
     //contextSafe sirve para generar animaciones 'seguras' en REACT para 'limpiar' (de la memoria) la animación una vez que useGSAP se haya montado y que la animación se haya ejecutado.
     
@@ -16,6 +20,31 @@ const Component5 = () => {
     const clickToEnlarge = contextSafe (() => {
         gsap.to('.component5-logo', {scale: 1.5, rotation: 90});
     });
+
+    //USO DE SCROLLTRIGGER:
+    useGSAP(() => {
+      gsap.to(".cuadrito", {
+        scrollTrigger: {
+          trigger: ".cuadrito",
+          start: 'top center',//La animación comienza cuando la parte superior (top) del elemento, pasa por el centro del viewport. Se pueden usar Px's
+          //end: 'bottom 100px', //Se pueden hacer marcadores relativos. Por ejemplo, el valor del marcador de fin, se puede hacer relativo al valor de inicio, por ejemplo +=300
+          end: '+=300',
+          markers: true, // despliega los marcadores de inicio/fin de la animación.
+          //Los start/end triggers, también pueden ser otros elementos del DOM
+          toggleActions: 'restart pause reverse pause'
+          //Keywords que se pueden usar en toggleActions: play pause resume reverse restart reset complete none
+          //Primera pos: hace la animación una vez entra al viewport, ejemplo: restart (la animación reinicia cada vez que se entra en el viewport).
+          //Segunda pos: lo que hace después de haber pasado el viewport (hecho scroll hacia más abajo), ejemplo: pause.
+          //Tercera pos: Se hace el ejemplo de'resume' y 'reverse' Cuando regresa al viewport de abajo hacia arriba.
+          //Cuarta pos: cuando se regresa al viewóprt de abajo hacia arriba, pero se sobrepasa éste, ejemplo usar pause.
+        },
+        x: 500,
+        rotation: 360,
+        duration: 3
+      });
+    });
+
+
 
 
     //Otra manera de poder crear una animación es con eventListener, aunque esto NO es buena práctica de uso en React (Recordar que al estar definitido el tipo de event, no es necesario definir un onClick en el elemento HTML correspondiente)
@@ -36,9 +65,13 @@ const Component5 = () => {
 
 
   return (
-    <div className='box' ref={clickBox}>
-      <img src={logo} alt='logo' className='component5-logo' onClick={clickToEnlarge} ref={atomImg}/>
-    </div>
+    <>
+      <div className='box' ref={clickBox}>
+        <img src={logo} alt='logo' className='component5-logo' onClick={clickToEnlarge} ref={atomImg}/>
+        <div id='cuadrito' className='cuadrito'>Cuadrito</div>
+      </div>
+
+    </>
   )
 }
 
